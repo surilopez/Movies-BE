b345fd9ba9506eb86f21b1733bbd196f25857f27
Emilio Suri Lopez <surilopez@gmail.com>
2020-12-09 17:28:53 -0500

    Some Repos was created

---
+++ Movies-BE/Repos/RepoOnMemory.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Movies_BE.Entities;
+
+namespace Movies_BE.Repos
+{
+    public class RepoOnMemory
+    {
+        private List<Genres> _genres;
+        public RepoOnMemory()
+        {
+            _genres = new List<Genres>()
+            {
+                new Genres(){id=1,Name = "Action"},
+                new Genres(){id=2,Name = "Drama"},
+            };
+        }
+
+
+        public List<Genres> GetGenres() {
+            return _genres;
+        }
+    }
+}
