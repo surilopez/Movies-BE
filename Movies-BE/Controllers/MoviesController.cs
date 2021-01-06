c374d2bd444ff54584eaaa6e8547c5b1d8fed8dc
Emilio Suri Lopez <surilopez@gmail.com>
2021-01-06 17:35:06 -0500

    MovieController and profile for automapper Movies  was added

--- Movies-BE/Controllers/MoviesController.cs
+++ Movies-BE/Controllers/MoviesController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Movies_BE.DTOs;
+using Movies_BE.Entities;
 using Movies_BE.Utilities;
 
 namespace Movies_BE.Controllers
@@ -13,14 +15,42 @@ namespace Movies_BE.Controllers
     public class MoviesController: ControllerBase
     {
         private readonly ApplicationDBContext context;
-        private readonly IMapper maper;
+        private readonly IMapper mapper;
         private readonly IStorageFile storageFile;
+        private readonly string container = "movies";
 
-        public MoviesController(ApplicationDBContext context, IMapper maper, IStorageFile storageFile)
+        public MoviesController(ApplicationDBContext context, IMapper mapper, IStorageFile storageFile)
         {
             this.context = context;
-            this.maper = maper;
+            this.mapper = mapper;
             this.storageFile = storageFile;
+        }
+
+        [HttpPost]
+
+        public async Task<ActionResult> Post([FromForm] MovieAddDTO movieAddDTO) {
+            var movie = mapper.Map<Movies>(movieAddDTO);
+
+            if (movieAddDTO.Img != null)
+            {
+                movie.Img = await storageFile.SaveFiles(container, movieAddDTO.Img);
+            }
+
+            WriteActorsByOrder(movie);
+            context.Add(movie);
+            await context.SaveChangesAsync();
+            return NoContent();
+
+        }
+
+        private void WriteActorsByOrder(Movies movie) {
+            if (movie.moviesActors!=null)
+            {
+                for (int i = 0; i < movie.moviesActors.Count; i++)
+                {
+                    movie.moviesActors[i].Order = i;
+                }
+            }
         }
     }
 }
--- Movies-BE/Utilities/AutoMappersProfiles.cs
+++ Movies-BE/Utilities/AutoMappersProfiles.cs
@@ -9,9 +9,9 @@ using NetTopologySuite.Geometries;
 
 namespace Movies_BE.Utilities
 {
-    public class AutoMappersProfiles:Profile
+    public class AutoMappersProfiles : Profile
     {
-        public AutoMappersProfiles(GeometryFactory geometryFactory )
+        public AutoMappersProfiles(GeometryFactory geometryFactory)
         {
             CreateMap<Genres, GenresDTO>().ReverseMap();
             CreateMap<GenresAddDTO, Genres>();
@@ -26,6 +26,63 @@ namespace Movies_BE.Utilities
             CreateMap<Theaters, TheaterDTO>()
                 .ForMember(x => x.Latitude, dto => dto.MapFrom(field => field.Location.Y))
                 .ForMember(x => x.Longitude, dto => dto.MapFrom(field => field.Location.X));
+
+            CreateMap<MovieAddDTO, Movies>()
+                .ForMember(x => x.Img, opt => opt.Ignore())
+                .ForMember(x => x.moviesGenres, opt => opt.MapFrom(MapMoviesGenres))
+                .ForMember(x => x.moviesTheaters, opt => opt.MapFrom(MapMoviesTheaters))
+                .ForMember(x => x.moviesActors, opt => opt.MapFrom(MapMoviesActors));
+
+        }
+
+        private List<MoviesActors> MapMoviesActors(MovieAddDTO movieAddDTO, Movies movie)
+        {
+            var result = new List<MoviesActors>();
+
+            if (movieAddDTO.ActorsList == null)
+            {
+                return result;
+            }
+            foreach (var item in movieAddDTO.ActorsList)
+            {
+                result.Add(new MoviesActors() { ActorID = item.id, Character = item.Character });
+            }
+
+            return result;
+        }
+
+        private List<MoviesGenres> MapMoviesGenres(MovieAddDTO movieAddDTO, Movies movie)
+        {
+
+            var result = new List<MoviesGenres>();
+
+            if (movieAddDTO.GenresIDList == null)
+            {
+                return result;
+            }
+            foreach (var item in movieAddDTO.GenresIDList)
+            {
+                result.Add(new MoviesGenres() { GenreID = item });
+            }
+
+            return result;
+        }
+
+        private List<MoviesTheaters> MapMoviesTheaters(MovieAddDTO movieAddDTO, Movies movie)
+        {
+
+            var result = new List<MoviesTheaters>();
+
+            if (movieAddDTO.TheatersIDList == null)
+            {
+                return result;
+            }
+            foreach (var item in movieAddDTO.TheatersIDList)
+            {
+                result.Add(new MoviesTheaters() { TheaterID = item });
+            }
+
+            return result;
         }
     }
 }
